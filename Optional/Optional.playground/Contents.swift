302a8ec39f249b9b2da3a26b10abea293a26714d
yakupacs <ykpacs@gmail.com>
2023-09-28 13:07:08 +0300

    Optional added.

---
+++ Optional/Optional.playground/Contents.swift
@@ -0,0 +1,43 @@
+var password = "1234"
+let passcode = Int(password)
+print("The passcode of the app is \(passcode!).")
+password = "hello world"
+if let code = Int(password) {
+  print("The passcode of the app is \(code).")
+} else {
+  print("Invalid passcode!")
+}
+let accessCode: Int
+if let code = Int(password) {
+  accessCode = code
+} else {
+  accessCode = 1111
+}
+print("The passcode of the app is \(accessCode).")
+let firstPassword = "hello"
+let secondPassword = "world"
+if let firstPasscode = Int(firstPassword),
+  let secondPasscode =
+	Int(secondPassword)
+{
+  print(
+	"The first passcode of the app is \(firstPasscode) and the second passcode of the app is \(secondPasscode)."
+  )
+} else {
+  print("Invalid passcodes!")
+}
+let firstAccessCode: Int
+let secondAccessCode: Int
+if let firstPasscode = Int(firstPassword),
+  let secondPasscode =
+	Int(secondPassword)
+{
+  firstAccessCode = firstPasscode
+  secondAccessCode = secondPasscode
+} else {
+  firstAccessCode = 1111
+  secondAccessCode = 2222
+}
+print(
+  "The first passcode of the app is \(firstAccessCode) and the second passcode of the app is \(secondAccessCode)."
+)
